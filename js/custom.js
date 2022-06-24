816d78b85763e81f4112ad762a3f8a6708b3e2c2
Memory77 <97756719+Memory77@users.noreply.github.com>
2022-06-24 14:24:55 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    
+    let randomNumber = Math.floor(Math.random() * 10)
+    console.log(randomNumber)
+
+    $("#formUser").on("submit" , function(e){
+        e.preventDefault()
+        let input = $("input[type=text]")
+        let userValue = parseInt(input.val())
+        if(randomNumber !== userValue){
+            input.val("")
+            input.addClass("error animated bounce")
+
+            setTimeout(function(){
+                input.removeClass("error animated bounce")
+            }, 1000)
+        }
+        if(randomNumber === userValue){
+            console.log("gagné")
+            input.remove()
+            $(".fa-check-circle").show()
+            $(".fa-check-circle").addClass("animated jackInTheBox")
+        }
+        
+    })
+
+})
